8f5f3d55b5b26e81b7bd2ca3e60000fad3f89424
Daniel Tian <dtian.95@gmail.com>
2021-05-17 18:13:13 +0800

    fixes

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 ## 真格日报 Automated!
 
 ### Introduction
+
 - An automation tool developed with Python and JS that helps automate the process of creating 真格日报, a highly structralized report that requires heavy repetitve work.
 - Primary data sources used are:
-    - 企名片 （融资新闻）
-    - 36kr（大公司新闻、深度分享）
+  - 企名片 （融资新闻）
+  - 36kr（大公司新闻、深度分享）
 
 ### Usage
+
 - Installation
   - Clone this repo by running `git clone https://github.com/KevinYu2050/ZhenDailyReport.git`
-  - Install the respective Python dependencies by running `pip install -r requirements.txt` 
+  - Install the respective Python dependencies by running `pip install -r requirements.txt`
 - Execution
   - Change the `UNION_ID` field in `rongzi_news.py` to the current session union id for 企名片, as shown in the following image:
-    ![image](images/example1.png)
+    ![image](images/example1.png 'Example')
   - Run `python rongzi_news.py`
--- rongzi_news.py
+++ rongzi_news.py
@@ -7,46 +7,51 @@ import ast
 from datetime import datetime
 
 
-
-UNION_ID = "oDeAKQ+7JjDhaBPNN5yeDYHbNBJtOMdy+0m+i2fvHuGAGbdcl144UAZrjuk4uynreJWqqIs6kiQsM8IbOYgM5A==" # Needs to be changed everytime
+# Needs to be changed everytime
+UNION_ID = "+0b5plWXT2VxnJh0ukCF5AFPT4MOsF1SMjLLZyz46pZefcz7eIATKkhYTrIUv2zFeJWqqIs6kiQsM8IbOYgM5A=="
 tag_dict = {
-    "企业服务":[],
-    "医疗健康":[],
-    "生产制造":[],
-    "人工智能":[],
-    "食品饮料":[],
-    "电子商务":[],
-    "金融":[],
-    "文娱传媒":[],
-    "教育培训":[],
-    "生活服务":[],
-    "服装纺织":[],
-    "消费升级":[]
+    "企业服务": [],
+    "医疗健康": [],
+    "生产制造": [],
+    "人工智能": [],
+    "食品饮料": [],
+    "电子商务": [],
+    "金融": [],
+    "文娱传媒": [],
+    "教育培训": [],
+    "生活服务": [],
+    "服装纺织": [],
+    "消费升级": [],
+    "物流运输": [],
+    "先进制造": []
 }
 
+
 def get_qimingpian_url():
-    url =  "https://vipapi.qimingpian.com/DataList/investEventVip"
+    url = "https://vipapi.qimingpian.com/DataList/investEventVip"
     page = 0
     df_concat = []
     while page <= 4:
         page += 1
-        payload =  {"page":page, "unionid":UNION_ID}
+        payload = {"page": page, "unionid": UNION_ID}
         response = requests.post(url, data=payload)
         res_json = json.loads(response.text)
-        # print(res_json)
+
+        # if res_json["status"] != 0:
+        #     break
+
+        print(res_json)
         decrypt_data = decrypt(res_json["encrypt_data"])
         json_data = json.loads(decrypt_data)["list"]
-        # print(pd.DataFrame(json_data))
         df_concat.append(pd.DataFrame(json_data))
-        # print(json_data)
 
     return pd.concat(df_concat)
 
- 
- 
+
 def decrypt(encrypt_data):
     ctx = execjs.compile(open('test.js').read())
     return ctx.call('my_decrypt', encrypt_data)
+
 
 def proc_data(df, date):
     for index, row in df.iterrows():
@@ -60,7 +65,7 @@ def proc_data(df, date):
             ret_string += row["product"]
             ret_string += "」"
             ret_string += "完成由"
-            
+
             lingtou = []
             dutou = []
             hetou = []
@@ -76,7 +81,6 @@ def proc_data(df, date):
                 elif inv["invest_type_name"] == "合投":
                     hetou.append(inv["investor"])
 
-            
             ret_string += str_format(lingtou, "领投")
             ret_string += str_format(gentou, "跟投")
             ret_string += str_format(dutou, "独投")
@@ -93,30 +97,35 @@ def proc_data(df, date):
 
     return tag_dict
 
+
 def get_36kr_news():
     url = "https://gateway.36kr.com/api/mis/nav/newsflash/flow"
-    response = requests.post(url, headers={"content-type": "application/json"}, data=json.dumps({"partner_id":"web","param":{"pageSize":50,"pageEvent":1,"pageCallback":"eyJmaXJzdElkIjoxMjE4MjI0NjkzMzM0NDA1LCJsYXN0SWQiOjEyMTgwNDQzMzE0NjMwNDQsImZpcnN0Q3JlYXRlVGltZSI6MTYyMDYyNjUzNDUwNSwibGFzdENyZWF0ZVRpbWUiOjE2MjA2MTU1MjYwOTB9","siteId":1,"platformId":2}}))
+    response = requests.post(url, headers={"content-type": "application/json"}, data=json.dumps({"partner_id": "web", "param": {"pageSize": 50, "pageEvent": 1,
+                                                                                                                                "pageCallback": "eyJmaXJzdElkIjoxMjE4MjI0NjkzMzM0NDA1LCJsYXN0SWQiOjEyMTgwNDQzMzE0NjMwNDQsImZpcnN0Q3JlYXRlVGltZSI6MTYyMDYyNjUzNDUwNSwibGFzdENyZWF0ZVRpbWUiOjE2MjA2MTU1MjYwOTB9", "siteId": 1, "platformId": 2}}))
     items = json.loads(response.text)["data"]["itemList"]
     news_list = []
 
     for item in items:
         content = item["templateMaterial"]["widgetContent"]
         if content.find("融资") == -1:
-            news_list.append(content.replace("36氪获悉，", "").replace("截至发稿，",""))
+            news_list.append(content.replace(
+                "36氪获悉，", "").replace("截至发稿，", ""))
 
     return news_list
 
+
 def get_36kr_deep_news():
     url = "https://gateway.36kr.com/api/mis/page/theme/flow"
-    response = requests.post(url, headers={"content-type": "application/json"}, data=json.dumps({"partner_id":"web","param":{"itemId":"327690223617","pageSize":1,"pageEvent":1,"pageCallback":"eyJmaXJzdElkIjo5MTEyOTgsImxhc3RJZCI6OTAxMTU2LCJmaXJzdENyZWF0ZVRpbWUiOjE2MDIzMjUyNjk3ODIsImxhc3RDcmVhdGVUaW1lIjoxNTk5Mjc3NjA2MTM2fQ","siteId":1,"platformId":2}}))
+    response = requests.post(url, headers={"content-type": "application/json"}, data=json.dumps({"partner_id": "web", "param": {"itemId": "327690223617", "pageSize": 1, "pageEvent": 1,
+                                                                                                                                "pageCallback": "eyJmaXJzdElkIjo5MTEyOTgsImxhc3RJZCI6OTAxMTU2LCJmaXJzdENyZWF0ZVRpbWUiOjE2MDIzMjUyNjk3ODIsImxhc3RDcmVhdGVUaW1lIjoxNTk5Mjc3NjA2MTM2fQ", "siteId": 1, "platformId": 2}}))
     items = json.loads(response.text)["data"]["itemList"]
     news_list = []
 
     for item in items:
-        news_list.append((item["templateMaterial"]["content"], item["templateMaterial"]["itemId"]))
+        news_list.append(
+            (item["templateMaterial"]["content"], item["templateMaterial"]["itemId"]))
 
     return news_list
-
 
 
 def compile_page(tag_dict, news_list, deep_news):
@@ -143,9 +152,6 @@ def compile_page(tag_dict, news_list, deep_news):
         page += "https://www.36kr.com/p/{} \n\n".format(news[1])
         count += 1
 
-
-
-
     return page
 
 
@@ -156,7 +162,6 @@ def str_format(li, inv_type):
         return ""
 
 
-
 if __name__ == '__main__':
     date = datetime.today().strftime("%Y%m%d")
     # print(date)
@@ -165,9 +170,8 @@ if __name__ == '__main__':
     df_news.to_csv("./data/{}.csv".format(date))
     df_news = pd.read_csv("./data/{}.csv".format(date))
     tag_dict = proc_data(df_news, date)
-    news_list = get_36kr_news() 
+    news_list = get_36kr_news()
     deep_news = get_36kr_deep_news()
-
 
     page = compile_page(tag_dict, news_list, deep_news)
     with open("./pages/{}.txt".format(date), "w") as f:
